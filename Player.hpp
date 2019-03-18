0063721d4b66f90b5abc57079bb10391aed8c3f1
marsathoriq <45200071+marsathoriq@users.noreply.github.com>
2019-03-18 18:01:49 +0700

    Add files via upload

---
+++ Player.hpp
@@ -0,0 +1,30 @@
+#include 'LinkedList'
+#include <iostream>
+class Player{
+	private:
+		int water // Jumlah kapasitas gembor (Alat penyiraman) yang tersisa untuk menyiram rumput
+		int money // Jumlah uang yang dimiliki player
+		int x,y; // Posisi Absis dan Ordinat player
+		LinkedList<Product> bag; // List yang menyimpan produk yang dihasilkan dari hewan
+	public:
+		//getter
+		int getWater() const; //mengambil atribut water
+		int getMoney() const; //mengambil atribut money
+		int getX() const; //mengambil atribut x (posisi Absis player)
+		int getY() const; //mengambil atribut y (posisi Ordinat player)
+		Product getBag(int idx) const; ////mengambil Product yang ada dalam LinkedList Bag pada indeks idx
+		//setter
+		void setWater(); //mengubah nilai atribut water
+		void setMoney(); //mengubah nilai atribut money
+		void setX(); //mengubah nilai atribut x (posisi Absis player)
+		void setY();//mengubah nilai atribut y (posisi Ordinat player)
+		void setBag(int idx); //mengubah nilai atribut LinkedList pada indeks idx
+		
+		void playerMove(char move); //Memindahkan posisi player ke atas / kanan / bawah / kiri dengan parameter input charr u/r/d/l dan posisi harus valid (harus divalidasi dulu)
+		void delBag(string NamaProduct); //Menghapus product dari bag dengan parameter input NamaProduct
+		void delBagAll(); //Mengosongkan Bag
+		void reduceWater(); //Mengkurangi atribut water yang ada didalam gebor (Alat penyiraman)
+		void addBag(Product Prod); //Menambahkan Product Prod ke Bag dan ditaruh di paling belakang
+		void addWater(); //Mengisi gebor dengan air secara maksimal (sesuai kapasitas) dengan mengubah atribut water 
+		void addMoney(int harga); //Menambah atribut money sesuai dengan harga
+};
