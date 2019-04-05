3246515d8c5e808821ffc80657ba1ba1fb7e1978
marsathoriq <45200071+marsathoriq@users.noreply.github.com>
2019-04-05 17:32:04 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,70 +13,100 @@ int main(){
 		P.print();
 		cin>>cmd;
 		if(cmd=="u"){
-
-			P.playerMove('u');
+			if (AC.noAnimalOn(Position(P.getX(),P.getY()))&&R.isValidPos(P.getX(),P.getY())){
+				P.playerMove('u');	
+			}else{
+				cout<<"Invalid Move"<<endl;
+			}
 		}else if(cmd=="l"){
-			P.playerMove('l');
+			if (AC.noAnimalOn(Position(P.getX(),P.getY()))&&R.isValidPos(P.getX(),P.getY())){
+				P.playerMove('l');	
+			}else{
+				cout<<"Invalid Move"<<endl;
+			}
 		}else if(cmd=="d"){
-			P.playerMove('d');
+			if (AC.noAnimalOn(Position(P.getX(),P.getY()))&&R.isValidPos(P.getX(),P.getY())){
+				P.playerMove('d');	
+			}else{
+				cout<<"Invalid Move"<<endl;
+			}
 		}else if(cmd=="r"){
-			P.playerMove('r');
-		} else if(cmd=="kill") {
+			if (AC.noAnimalOn(Position(P.getX(),P.getY()))&&R.isValidPos(P.getX(),P.getY())){
+				P.playerMove('r');	
+			}else{
+				cout<<"Invalid Move"<<endl;
+			}
+		}else if(cmd=="kill"){
 			//udah dapet hewan x
+			bool Killed=false;
 			if(!AC.noAnimalNear()){
-				if(!AC.noAnimalOn(Position(Pos.getAbsis()-1,Pos.getOrdinat()))){
+				if(!AC.noAnimalOn(Position(Pos.getAbsis()-1,Pos.getOrdinat()))&&!Killed){
 					if (AC.findOnPos(Position(Pos.getAbsis()-1,Pos.getOrdinat()))->canBeKilled()){
 						P.addProduct(AC.findOnPos(Position(Pos.getAbsis()-1,Pos.getOrdinat()))->killProduct());
-					}
-				}else if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()-1))){
+						AC.removeAnimal(AC.findOnPos(Position(Pos.getAbsis()-1,Pos.getOrdinat())));
+						Killed=true;
+					}	
+				}if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()-1))&&!Killed){
 					if (AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()-1))->canBeKilled()){
 						P.addProduct(AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()-1))->killProduct());
+						Killed=true;
 					}
-				}else if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()+1))){
+				}if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()+1))&&!Killed){
 					if (AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->canBeKilled()){
 						P.addProduct(AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->killProduct());
+						Killed=true;
 					}
-				}else if(!AC.noAnimalOn(Position(Pos.getAbsis()+1,Pos.getOrdinat()))){
+				}if(!AC.noAnimalOn(Position(Pos.getAbsis()+1,Pos.getOrdinat()))&&!Killed){
 					if (AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->canBeKilled()){
 						P.addProduct(AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->killProduct());
+						Killed=true;
 					}
 				}
 			}else{
 				cout<<"Tidak ada hewan yang bisa dibunuh"<<endl;
 			}
 		}else if(cmd=="interact"){
+			bool Interact = false;
 			if(!AC.noAnimalNear()){
-				if(!AC.noAnimalOn(Position(Pos.getAbsis()-1,Pos.getOrdinat()))){
+				if(!AC.noAnimalOn(Position(Pos.getAbsis()-1,Pos.getOrdinat()))&&!Interact){
 					if (AC.findOnPos(Position(Pos.getAbsis()-1,Pos.getOrdinat()))->canBeInteracted()){
 						P.addProduct(AC.findOnPos(Position(Pos.getAbsis()-1,Pos.getOrdinat()))->interactProduct());
-					}
-				}else if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()-1))){
+						Interact = true;
+					}	
+				}if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()-1))&&!Interact){
 					if (AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()-1))->canBeInteracted()){
 						P.addProduct(AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()-1))->interactProduct());
+						Interact = true;
 					}
-				}else if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()+1))){
+				}if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()+1))&&!Interact){
 					if (AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->canBeInteracted()){
 						P.addProduct(AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->interactProduct());
+						Interact = true;
 					}
-				}else if(!AC.noAnimalOn(Position(Pos.getAbsis()+1,Pos.getOrdinat()))){
+				}if(!AC.noAnimalOn(Position(Pos.getAbsis()+1,Pos.getOrdinat()))&&!Interact){
 					if (AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->canBeInteracted()){
 						P.addProduct(AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->interactProduct());
+						Interact = true;
 					}
 				}
-			}else{
-				cout<<""<<endl;
+			}if (R.isNear(P.getX(),P.getY(),"Well")&&!Interact) {
+				P.addWater();
+			}else if (R.isNear(P.getX(),P.getY(),"Truck")){
+				P.delProductAll();
+			}else if (R.isNear(P.getX(),P.getY(),"Mixer")){
+					
 			}
 		}else if(cmd=="talk"){
 			Position Pos(P.getX(),P.getY());
 			if(!AC.noAnimalNear(Pos)){
 				if(!AC.noAnimalOn(Position(Pos.getAbsis()-1,Pos.getOrdinat()))){
-					AC.findOnPos(Position(Pos.getAbsis()-1,Pos.getOrdinat()))->sound();
+					cout<<AC.findOnPos(Position(Pos.getAbsis()-1,Pos.getOrdinat()))->animalType()<<" bersabda	:"<<AC.findOnPos(Position(Pos.getAbsis()-1,Pos.getOrdinat()))->sound()<<endl;	
 				}else if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()-1))){
-					AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()-1))->sound();
+					cout<<AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()-1))->animalType()<<" bersabda	:"<<AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()-1))->sound()<<endl;
 				}else if(!AC.noAnimalOn(Position(Pos.getAbsis(),Pos.getOrdinat()+1))){
-					AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->sound();
+					cout<<AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->animalType()<<" bersabda	:"<<AC.findOnPos(Position(Pos.getAbsis(),Pos.getOrdinat()+1))->sound()<<endl;
 				}else if(!AC.noAnimalOn(Position(Pos.getAbsis()+1,Pos.getOrdinat()))){
-					AC.findOnPos(Position(Pos.getAbsis()+1,Pos.getOrdinat()))->sound();
+					cout<<AC.findOnPos(Position(Pos.getAbsis()+1,Pos.getOrdinat()))->animalType()<<" bersabda	:"<<AC.findOnPos(Position(Pos.getAbsis()+1,Pos.getOrdinat()))->sound()<<endl;
 			}else{
 				cout<<"Tidak ada hewan disekitar"<<endl;
 			}
@@ -90,5 +120,8 @@ int main(){
 		}else{
 			cout<<"perintah salah"<<endl;
 		}
+		AC.allAnimalMove();
+		AC.allAnimalHungrier();
+		
 	}
 }
