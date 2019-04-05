2d85485dbfc62bb463d4f9bacaec12964b6205f3
naufalzhafran <latifnaufal7@gmail.com>
2019-04-05 17:42:12 +0700

    fix facility

--- Render/Facility.cpp
+++ Render/Facility.cpp
@@ -1,10 +1,21 @@
 #include "Facility.hpp"
 
 /** Constructor */
-Facility::Facility(int x, int y, std::string legend): Cell(x,y,legend)
+Facility::Facility(int x, int y, std::string legend) : Cell(x, y, legend)
 {
-
 }
 
+bool isGrass()
+{
+    return false;
+}
 
+void growGrass()
+{
+    /** DO NOTHING */
+}
 
+void eatGrass()
+{
+    /** DO NOTHING */
+}
